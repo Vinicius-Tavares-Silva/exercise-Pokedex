5f7bd9adfce65acc44aba6952d8e2bc4518597b5
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-22 18:36:09 -0300

    Cria o Componente Pokedex que gera a lista de Pokemons

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Pokemon from './components/Pokemon'
+import Pokedex from './components/Pokedex'
 
 function App() {
   return (
     <div className="App">
-      <Pokemon />
+      <Pokedex />
     </div>
   );
 }
--- pokedex/src/components/Pokemon.js
+++ pokedex/src/components/Pokemon.js
@@ -3,14 +3,15 @@ import pokemons from '../data';
 
 class Pokemon extends React.Component {
  render() {
+  const {pokemonInfo} = this.props;
   return (
-   <div class="card" style={{width: "18rem"}}>
-  <img src={pokemons[0].image} class="card-img-top" alt="..."/>
+   <div class="card" style={{width: "15rem"}}>
+  <img src={pokemonInfo.image} class="card-img-top" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">{pokemons[0].name}</h5>
-    <p class="card-text">{pokemons[0].type}</p>
-    <p class="card-text">Average weight: {pokemons[0].averageWeight.value} {pokemons[0].averageWeight.measurementUnit}</p>
-    <a target="_blank" href={pokemons[0].moreInfo} class="btn btn-primary">More Info!</a>
+    <h4 class="card-title">{pokemonInfo.name}</h4>
+    <p class="card-text">{pokemonInfo.type}</p>
+    <p class="card-text">Average weight: {pokemonInfo.averageWeight.value} {pokemonInfo.averageWeight.measurementUnit}</p>
+    <a target="_blank" href={pokemonInfo.moreInfo} class="btn btn-primary">More Info!</a>
   </div>
 </div>
   );
