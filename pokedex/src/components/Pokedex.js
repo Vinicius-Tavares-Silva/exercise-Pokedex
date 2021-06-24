91a8d6c65e86e87303b692fc679d562405c74de6
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-06-24 19:36:20 -0300

    Adiciona botoes para filtro

--- pokedex/src/components/Pokedex.js
+++ pokedex/src/components/Pokedex.js
@@ -1,18 +1,63 @@
 import React from 'react';
-import Pokemon from './Pokemon'
+import Pokemon from './Pokemon';
 import pokemons from '../data';
 
 class Pokedex extends React.Component {
- render(){
-  return (
-   <section>
-    {
-     pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemonInfo={pokemon}/>)
-    }
-   </section>
+  constructor() {
+    super();
+    this.state = {
+      pokemonTypeFilter: 'All',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  );
- }
+  handleClick = (event) => {
+    this.setState({ pokemonTypeFilter: event.target.innerText });
+  };
+
+  render() {
+    const { pokemonTypeFilter } = this.state;
+    return (
+      <div>
+        <section>
+          <button className="btn btn-danger" onClick={this.handleClick}>
+            All
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Fire
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Electric
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Bug
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Poison
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Psychic
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Normal
+          </button>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            Dragon
+          </button>
+        </section>
+        <section>
+          {pokemons
+            .filter((pokemon) => {
+              if (pokemonTypeFilter === 'All') return true;
+              return pokemon.type === pokemonTypeFilter;
+            })
+            .map((pokemon) => (
+              <Pokemon key={pokemon.id} pokemonInfo={pokemon} />
+            ))}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
--- pokedex/src/components/Pokemon.js
+++ pokedex/src/components/Pokemon.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import pokemons from '../data';
 
 class Pokemon extends React.Component {
  render() {
   const {pokemonInfo} = this.props;
   return (
-   <div class="card" style={{width: "15rem"}}>
-  <img src={pokemonInfo.image} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h4 class="card-title">{pokemonInfo.name}</h4>
-    <p class="card-text">{pokemonInfo.type}</p>
-    <p class="card-text">Average weight: {pokemonInfo.averageWeight.value} {pokemonInfo.averageWeight.measurementUnit}</p>
-    <a target="_blank" href={pokemonInfo.moreInfo} class="btn btn-outline-info">More Info!</a>
+   <div className="card" style={{width: "15rem"}}>
+  <img src={pokemonInfo.image} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h4 className="card-title">{pokemonInfo.name}</h4>
+    <p className="card-text">{pokemonInfo.type}</p>
+    <p className="card-text">Average weight: {pokemonInfo.averageWeight.value} {pokemonInfo.averageWeight.measurementUnit}</p>
+    <a target="_blank" href={pokemonInfo.moreInfo} className="btn btn-outline-info">More Info!</a>
   </div>
 </div>
   );
